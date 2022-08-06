7afe8fde4f7bd72ba8fbe3570dc327ddf6e2c1c6
Efe Sunuc <efe@retux.io>
2022-08-06 12:20:17 +0300

    create new function for js

--- louise.js
+++ louise.js
@@ -1,1 +1,7 @@
 console.log('Hello World');
+
+// <editor-fold desc="created a new function for adding two given parametres.">
+const myAdderFunc = function(x,y){
+    return x + y;
+}
+// </editor-fold>
