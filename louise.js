20d2aa77fb39a97b5d2efc3f105ed273f19594eb
Guest <takeanxiety95@gmail.com>
2022-09-18 11:17:11 +0300

    chore: complete all 4 functions

--- louise.js
+++ louise.js
@@ -1,28 +1,23 @@
-console.log('Hello World');
-
-// <editor-fold desc="created a new function for adding two given parametres">
-const myAdderFunc = function(x,y){
-    const result = x + y;
-    return result;
-}
-// </editor-fold>
-
-// <editor-fold desc="created a new function for expirement">
-const myExpFunc = function(x){
-    for(let i = 0; i < 10; i++){
-        console.log(x);
-    }
-}
-// </editor-fold>
-
-// <editor-fold desc="created a new function for addin two numbers together">
+// <editor-fold desc="created a new function for adding two numbers together">
 const addTwoNum = function(x,y){
     return x + y;
+}
+// </editor-fold>
+
+// <editor-fold desc="created a new function for subtracting two numbers together">
+const subTwoNum = function(x,y){
+    return x - y;
 }
 // </editor-fold>
 
 // <editor-fold desc="created a new function for multiplying given two numbers">
 const multTwoNum = function(x,y){
     return x * y;
+}
+// </editor-fold>
+
+// <editor-fold desc="created a new function for dividing given two numbers">
+const divTwoNum = function(x,y){
+    return x / y;
 }
 // </editor-fold>
