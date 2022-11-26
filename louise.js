0298c570d28ca2d165074022470a3dd2fd95bcd6
Unknown <takeanxiety95@gmail.com>
2022-11-26 19:37:38 +0300

    chore: remove unneccesary function

--- louise.js
+++ louise.js
@@ -39,10 +39,4 @@ const wyattBranch = function(){
     console.log('wyatt branch');
 }
 // </editor-fold>
-
-// <editor-fold desc="added new function for efe branch">
-const efeBranch = function(){
-    console.log('efe branch');
-}
-// </editor-fold>
-
+    
